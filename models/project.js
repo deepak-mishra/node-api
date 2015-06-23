59ffb54f2a232f81a3e03e46031a1ed9a9978eb6
Dee <pt.deepakmishra@gmail.com>
2015-06-23 17:31:22 +0530

    Update project.js

--- models/project.js
+++ models/project.js
@@ -1,1 +1,12 @@
-test
+// Load required packages
+var mongoose = require('mongoose');
+
+// Define our beer schema
+var ProjectSchema   = new mongoose.Schema({
+  title: String,
+  url: String,
+  description: String
+});
+
+// Export the Mongoose model
+module.exports = mongoose.model('Project', ProjectSchema);
